226db528db33c20fa7a5b7cb32a12cd241a8c230
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2021-06-09 23:07:58 +0530

    Initial commit

---
+++ app/src/main/java/com/aurosaswatraj/betacalculator/B_calculator.kt
@@ -0,0 +1,11 @@
+package com.aurosaswatraj.betacalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class B_calculator : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.simple_calculator)
+    }
+}
